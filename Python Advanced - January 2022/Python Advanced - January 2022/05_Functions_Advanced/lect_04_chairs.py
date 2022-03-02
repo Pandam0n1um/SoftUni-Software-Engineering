5b298526bc9611ac06e6a6801d3e5ec1d55ebc44
Todor Todorov <t_todorof@protonmail.com>
2022-03-03 00:11:10 +0200

    advanced-fifth topic commited

---
+++ Python Advanced - January 2022/Python Advanced - January 2022/05_Functions_Advanced/lect_04_chairs.py
@@ -0,0 +1,19 @@
+# from itertools import combinations
+#
+# print(combinations(input().split(", ").int(input())))
+
+
+def combinations(names, count, current_names=[]):
+    if len(current_names) == count:
+        print(", ".join(current_names))
+        return
+
+    for i in range(len(names)):
+        current_names.append(names[i])
+        combinations(names[i + 1:], count, current_names)
+        current_names.pop()
+
+
+names = input().split(", ")
+n = int(input())
+combinations(names, n)
