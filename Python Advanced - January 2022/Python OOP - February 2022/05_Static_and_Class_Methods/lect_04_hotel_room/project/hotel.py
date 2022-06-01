ebf4f7da6b1c7101afe9313f40417145c5d04370
Todor Todorov <t_todorof@protonmail.com>
2022-06-01 21:14:07 +0300

    OOP-fifth topic commited

---
+++ Python Advanced - January 2022/Python OOP - February 2022/05_Static_and_Class_Methods/lect_04_hotel_room/project/hotel.py
@@ -0,0 +1,30 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        results = room.take_room(people)
+        if not results:
+            self.guests += people
+
+    def free_room(self, room_number):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        self.guests -= room.guests
+        room.free_room()
+
+    def status(self):
+        numbers_free_rooms = [str(room.number) for room in self.rooms if not room.is_taken]
+        numbers_taken_rooms = [str(room.number) for room in self.rooms if room.is_taken]
+        return f"Hotel {self.name} has {self.guests} total guests" + '\n' \
+                f"Free rooms: {', '.join(numbers_free_rooms)}" + '\n' \
+                f"Taken rooms: {', '.join(numbers_taken_rooms)}"
