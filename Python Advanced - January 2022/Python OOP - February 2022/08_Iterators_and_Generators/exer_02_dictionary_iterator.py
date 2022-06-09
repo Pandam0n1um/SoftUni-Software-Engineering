3628d074bc87887a166367d14c55f1a8355f9318
Todor Todorov <t_todorof@protonmail.com>
2022-06-10 02:19:26 +0300

    OOP-eigth topic commited

---
+++ Python Advanced - January 2022/Python OOP - February 2022/08_Iterators_and_Generators/exer_02_dictionary_iterator.py
@@ -0,0 +1,19 @@
+class dictionary_iter:
+	def __init__(self, dict_input: dict):
+		self.dict_input = list(dict_input.items())
+		self.index = 0
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.index >= len(self.dict_input):
+			raise StopIteration
+		result=self.dict_input[self.index]
+		self.index+=1
+		return result
+
+a=5
+result = dictionary_iter({1: "1", 2: "2"})
+for x in result:
+	print(x)
