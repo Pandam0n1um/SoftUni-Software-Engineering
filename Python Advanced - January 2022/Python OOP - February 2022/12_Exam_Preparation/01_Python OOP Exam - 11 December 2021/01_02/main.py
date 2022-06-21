a518c2922728d7c6b4194475cc828f96158f6177
Todor Todorov <t_todorof@protonmail.com>
2022-06-21 21:55:38 +0300

    OOP-regular exam 2021-12-11 commited

---
+++ Python Advanced - January 2022/Python OOP - February 2022/12_Exam_Preparation/01_Python OOP Exam - 11 December 2021/01_02/main.py
@@ -0,0 +1,41 @@
+from project.controller import Controller
+#
+controller = Controller()
+print(controller.create_driver("Peter"))
+print(controller.create_car("SportsCar", "Porsche 718 Boxster", 470))
+print(controller.add_car_to_driver("Peter", "SportsCar"))
+print(controller.create_car("SportsCar", "Porsche 911", 580))
+print(controller.add_car_to_driver("Peter", "SportsCar"))
+print(controller.create_car("MuscleCar", "BMW ALPINA B7", 290))
+print(controller.create_car("MuscleCar", "Mercedes-Benz AMG GLA 45", 420))
+print(controller.create_driver("John"))
+print(controller.create_driver("Jack"))
+print(controller.create_driver("Kelly"))
+print(controller.add_car_to_driver("Kelly", "MuscleCar"))
+print(controller.add_car_to_driver("Jack", "MuscleCar"))
+print(controller.add_car_to_driver("John", "SportsCar"))
+print(controller.create_race("Christmas Top Racers"))
+print(controller.add_driver_to_race("Christmas Top Racers", "John"))
+print(controller.add_driver_to_race("Christmas Top Racers", "John"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Jack"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Kelly"))
+print(controller.add_driver_to_race("Christmas Top Racers", "Peter"))
+print(controller.start_race("Christmas Top Racers"))
+[print(d.name, d.number_of_wins) for d in controller.drivers]
+
+
+
+# from project.car.muscle_car import MuscleCar
+# from project.car.sports_car import SportsCar
+# from project.controller import Controller
+# from project.driver import Driver
+#
+# controller_my=Controller()
+#
+# muscle_car=MuscleCar('  ',255)
+#
+#
+# sport_car=SportsCar('Porschee',455)
+#
+#
+# driver=Driver('Joe')
